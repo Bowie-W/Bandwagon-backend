2b6345b967dc018ed15ef408b1f7eebd17070d82
BW <bowiewong12@gmail.com>
2022-12-03 12:58:47 -0800

    able to post to Users

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,7 +1,14 @@
 const knex = require("knex")(require("../knexfile"));
 
-exports.addNewUser = (req,res) => {
-
-
-
-}
+exports.addNewUser = (req, res) => {
+  knex("users")
+    .insert(req.body)
+    .then((newUserId) => {
+      res.status(201).json(newUserId[0]);
+    })
+    .catch(() => {
+      res.status(400).json({
+        message: `Error Adding User`,
+      });
+    });
+};
