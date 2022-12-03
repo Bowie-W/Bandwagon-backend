88ed0b1dd9f28933a750847681355fa2ee3cb573
BW <bowiewong12@gmail.com>
2022-12-03 12:17:49 -0800

    add knex connection and firstdatabase table *Users*

---
+++ migrations/20221203200958_users.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('users', (table) => {
+        table.uuid('id').primary();
+        table.string('username').notNullable()
+        table.string('email').notNullable()
+        table.string('city').notNullable()
+    })
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
